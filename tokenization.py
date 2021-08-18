fac58259c8ca70a4a209ef8df8e7fcdf7c425d97
Sayujya Malkan <sayujyamalkan@gmail.com>
2021-08-18 14:25:12 +0530

    Initial commit

---
+++ tokenization.py
@@ -0,0 +1,19 @@
+import nltk
+import re
+
+text1 = "Founded in 2002, SpaceX’s mission is to enable humans to become a spacefaring civilization and a multi-planet species by building a self-sustaining city on Mars. In 2008, SpaceX’s Falcon 1 became the first privately developed liquid-fuel launch vehicle to orbit the Earth."
+print(text1)
+
+#Tokenization using split() method
+
+splitFuncToken = text1.split()
+# print(splitFuncToken)
+
+# Sentence Tokenization
+sentenceSplitFuncToken = text1.split(".")
+# print(sentenceSplitFuncToken)
+
+# Tokenization Using Regex
+
+regexToken = re.findall("[\w]+",text1)
+print(regexToken)
